644092fec339a5526d7332e8141dedc4db420c6c
roo-gar <A01373670@itesm.mx>
2019-10-06 15:55:24 -0500

    Program and ConstantDeclaration methods

--- Lexical analysis/Parser.cs
+++ Lexical analysis/Parser.cs
@@ -13,6 +13,16 @@ namespace Chimera
 
     public class Parser
     {
+        private static readonly ISet<TokenCategory> firstOfStatement =
+            new HashSet<TokenCategory>() {
+                TokenCategory.IDENTIFIER,
+                TokenCategory.IF,
+                TokenCategory.LOOP,
+                TokenCategory.FOR,
+                TokenCategory.RETURN,
+                TokenCategory.EXIT
+            };
+
         private IEnumerator<Token> tokenStream;
 
         public Parser(IEnumerator<Token> tokenStream)
@@ -43,7 +53,7 @@ namespace Chimera
         /* 
          * LL(1) Grammar:
          *      PROGRAM ::= ("const" CONST_DECL+)? ("var" VAR_DECL+)? PROC_DECL* "program" STATEMENT* "end" ";"
-         *         
+         *      CONST_DECL ::= IDENTIFIER ":=" LITERAL ";"    
          * 
          * 
          * 
@@ -51,7 +61,68 @@ namespace Chimera
 
         public void Program()
         {
-            Console.WriteLine("Test");
+            if (CurrentToken == TokenCategory.CONST)
+            {
+                Expect(TokenCategory.CONST);
+                do
+                {
+                    ConstantDeclaration();
+                } while (CurrentToken == TokenCategory.IDENTIFIER);        
+            }
+
+            if (CurrentToken == TokenCategory.VAR)
+            {
+                Expect(TokenCategory.VAR);
+                do
+                {
+                    VariableDeclaration();
+                } while (CurrentToken == TokenCategory.IDENTIFIER);
+            }
+
+            while (CurrentToken == TokenCategory.PROCEDURE)
+            {
+                ProcedureDeclaration();
+            }
+
+            Expect(TokenCategory.PROGRAM);
+
+            while (firstOfStatement.Contains(CurrentToken))
+            {
+                Statement();
+            }
+
+            Expect(TokenCategory.END);
+            Expect(TokenCategory.SEMICOLON);
+            Expect(TokenCategory.EOF);
         }
+
+        public void ConstantDeclaration()
+        {
+            Expect(TokenCategory.IDENTIFIER);
+            Expect(TokenCategory.ASSIGN_CONST);
+            Literal();
+            Expect(TokenCategory.SEMICOLON);
+        }
+
+        public void VariableDeclaration()
+        {
+            return;
+        }
+
+        public void ProcedureDeclaration()
+        {
+            return;
+        }
+
+        public void Statement()
+        {
+            return;
+        }
+
+        public void Literal()
+        {
+            return;
+        }
+
     }
 }
