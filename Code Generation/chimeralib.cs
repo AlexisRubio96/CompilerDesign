6121cdef3228791939b2e18022d36ecc57ea2e16
roo-gar <A01373670@itesm.mx>
2019-11-27 22:39:13 -0600

    Print example

--- Code Generation/chimeralib.cs
+++ Code Generation/chimeralib.cs
@@ -1,32 +1,12 @@
 /*
-  Buttercup runtime library.
-  Copyright (C) 2013 Ariel Ortiz, ITESM CEM
-
-  To compile this module as a DLL:
-
-                mcs /t:library bcuplib.cs
-
-  To link this DLL to a program written in C#:
-
-                mcs /r:bcuplib.dll someprogram.cs
-
-  This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-  
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A01373670 - Rodrigo Garcia Lopez
+ * A01372074 - Jorge Alexis Rubio Sumano
+ * A01371084 - Valentin Ochoa Lopez
 */
 
 using System.Collections;
 
-namespace Buttercup {
+namespace Chimera {
     
     using System;
 
