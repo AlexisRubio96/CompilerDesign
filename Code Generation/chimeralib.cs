fabad5ecc92e7d1796f759687fc33532ea5dc929
roo-gar <A01373670@itesm.mx>
2019-11-29 22:18:27 -0600

    Fix library bug

--- Code Generation/chimeralib.cs
+++ Code Generation/chimeralib.cs
@@ -7,11 +7,13 @@
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Chimera {
-    
+namespace Chimera
+{
+
     using System;
 
-    public class Utils {
+    public class Utils
+    {
 
         public static void WrInt(int i)
         {
@@ -22,73 +24,102 @@ namespace Chimera {
         {
             Console.WriteLine(b);
         }
-         public static void WrLn( )
+
+        public static void WrLn()
         {
             Console.WriteLine();
         }
-       public static int RdInt(){
+
+        public static int RdInt()
+        {
             String input = Console.ReadLine();
-			int output;
-			bool good = Int32.TryParse(input,out output);
-			if(!good){
-				throw new ArgumentException("Invalid input, must be an int value.");
-			}
+            int output;
+            bool good = Int32.TryParse(input, out output);
+            if (!good)
+            {
+                throw new ArgumentException("Invalid input, must be an int value.");
+            }
             return output;
-            
+
         }
-        public static String RdStr() {
+
+        public static String RdStr()
+        {
             String input = Console.ReadLine();
             return input;
         }
-        public static char AtStr(String s, int i){
-			return s[i];
+
+        public static char AtStr(String s, int i)
+        {
+            return s[i];
         }
-        public static int LenStr(String s){
-			return s.Length;
+
+        public static int LenStr(String s)
+        {
+            return s.Length;
         }
-        public static int CmpStr(String s1, String s2){
-            return string.Compare(s1,s2);
+
+        public static int CmpStr(String s1, String s2)
+        {
+            return string.Compare(s1, s2);
         }
-        public static String CatStr(String s1,String s2){
-            return s1+s2;
+
+        public static String CatStr(String s1, String s2)
+        {
+            return s1 + s2;
         }
-      	 public static int LenLstInt(int[] lst){
+
+        public static int LenLstInt(int[] lst)
+        {
             return lst.Length;
         }
-         public static int LenLstStr(String[] lst){
+
+        public static int LenLstStr(String[] lst)
+        {
             return lst.Length;
         }
-        public static int LenLstBool(Boolean[] lst){
+
+        public static int LenLstBool(Boolean[] lst)
+        {
             return lst.Length;
         }
-       	public static int[] NewLstInt(int size){
+
+        public static int[] NewLstInt(int size)
+        {
             int[] L = new int[size];
             return L;
         }
-        public static String[] NewLstInt(int size){
+
+        public static String[] NewLstStr(int size)
+        {
             String[] L = new String[size];
-            for(int i = 0;i < L.Length; i++ ){
+            for (int i = 0; i < L.Length; i++)
+            {
                 L[i] = "";
             }
             return L;
         }
-       public static bool[] NewLstInt(int size){
+
+        public static bool[] NewLstBool(int size)
+        {
             bool[] L = new bool[size];
             return L;
         }
-}
-        public static String IntToStr(int i){
-		    return i.ToString();
+
+        public static String IntToStr(int i)
+        {
+            return i.ToString();
         }
-        public static int StrToInt(String s){
+
+        public static int StrToInt(String s)
+        {
             int i;
-		    Boolean good = Int32.TryParse(s, out i);
-		  	if(!good){
-				throw new ArgumentException("Invalid input, must be an int value.");
-			}
+            Boolean good = Int32.TryParse(s, out i);
+            if (!good)
+            {
+                throw new ArgumentException("Invalid input, must be an int value.");
+            }
             return i;
         }
-
-
     }
 }
